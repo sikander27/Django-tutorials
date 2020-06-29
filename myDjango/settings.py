bb17e8ef2a09b5596e208535b33899a94d035529
sikander khan <41859385+sikander27@users.noreply.github.com>
2020-06-29 20:56:19 +0530

    added procfile

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -142,3 +142,6 @@ EMAIL_USE_TLS = True
 # EMAIL_HOST_PASSWORD ='zmtlxewuiqqkgiwh'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+django_heroku.settings(locals())
